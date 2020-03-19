79a0f1d50fec1a3ae9d08341f1bf178007623db3
AdvAppSw <advappsw@gmail.com>
2020-03-18 19:17:29 -0700

    Moved Replacement into its own module

--- DataClasses/DailyReports.cs
+++ DataClasses/DailyReports.cs
@@ -164,16 +164,4 @@ namespace DataClasses
 
         #endregion
     }
-
-    class Replacement
-    {
-        public Replacement(string from, string to)
-        {
-            From = from;
-            To = to;
-        }
-
-        public string From { get; set; }
-        public string To { get; set; }
-    }
 }
---
+++ DataClasses/Replacement.cs
@@ -0,0 +1,14 @@
+﻿namespace DataClasses
+{
+    class Replacement
+    {
+        public Replacement(string from, string to)
+        {
+            From = from;
+            To = to;
+        }
+
+        public string From { get; set; }
+        public string To { get; set; }
+    }
+}
