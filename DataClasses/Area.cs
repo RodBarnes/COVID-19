b4ea021727955c69e202936d4d7d2498078a75b9
AdvAppSw <advappsw@gmail.com>
2020-03-21 18:08:40 -0700

    Moved Area class into its own module

---
+++ DataClasses/Area.cs
@@ -0,0 +1,55 @@
+﻿namespace DataClasses
+{
+    public class Area
+    {
+        public Area() { }
+
+        public Area(string region, string state, string district, int confirmed, int recovered, int deaths)
+        {
+            Region = region;
+            State = state;
+            District = district;
+            Confirmed = confirmed;
+            Recovered = recovered;
+            Deaths = deaths;
+        }
+        public string Region { get; set; }
+        public string State { get; set; }
+        public string District { get; set; }
+        public int Confirmed { get; set; }
+        public int Recovered { get; set; }
+        public int Deaths { get; set; }
+
+        public string RegionState
+        {
+            get
+            {
+                var result = Region;
+                if (!string.IsNullOrEmpty(State))
+                {
+                    result += $",{State}";
+                }
+
+                return result;
+            }
+        }
+
+        public string RegionStateDistrict
+        {
+            get
+            {
+                var result = Region;
+                if (!string.IsNullOrEmpty(State))
+                {
+                    result += $",{State}";
+                }
+                if (!string.IsNullOrEmpty(District))
+                {
+                    result += $",{District}";
+                }
+
+                return result;
+            }
+        }
+    }
+}
--- WpfViewer/ViewModels/MainVM.cs
+++ WpfViewer/ViewModels/MainVM.cs
@@ -5,7 +5,6 @@ using System.ComponentModel;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Windows.Media;
-using Common;
 using DataClasses;
 using LiveCharts;
 using LiveCharts.Wpf;
@@ -258,58 +257,5 @@ namespace WpfViewer.ViewModels
         }
 
         #endregion
-    }
-
-    public class Area
-    {
-        public Area() { }
-
-        public Area(string region, string state, string district, int confirmed, int recovered, int deaths)
-        {
-            Region = region;
-            State = state;
-            District = district;
-            Confirmed = confirmed;
-            Recovered = recovered;
-            Deaths = deaths;
-        }
-        public string Region { get; set; }
-        public string State { get; set; }
-        public string District { get; set; }
-        public int Confirmed { get; set; }
-        public int Recovered { get; set; }
-        public int Deaths { get; set; }
-
-        public string RegionState
-        {
-            get
-            {
-                var result = Region;
-                if (!string.IsNullOrEmpty(State))
-                {
-                    result += $",{State}";
-                }
-
-                return result;
-            }
-        }
-
-        public string RegionStateDistrict
-        {
-            get
-            {
-                var result = Region;
-                if (!string.IsNullOrEmpty(State))
-                {
-                    result += $",{State}";
-                }
-                if (!string.IsNullOrEmpty(District))
-                {
-                    result += $",{District}";
-                }
-
-                return result;
-            }
-        }
     }
 }
