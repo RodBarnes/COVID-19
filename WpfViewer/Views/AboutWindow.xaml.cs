630364c04bff1fd54b837732bada667b0a637c43
AdvAppSw <advappsw@gmail.com>
2020-04-03 13:02:19 -0700

    Adde AboutWindow to System menu of MainWindow

---
+++ WpfViewer/Views/AboutWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using Viewer.ViewModels;
+
+namespace Viewer
+{
+    /// <summary>
+    /// Interaction logic for AboutWindow.xaml
+    /// </summary>
+    public partial class AboutWindow : Window
+    {
+        public AboutWindow()
+        {
+            DataContext = new AboutVM();
+
+            InitializeComponent();
+        }
+    }
+}
--- WpfViewer/Views/MainWindow.xaml.cs
+++ WpfViewer/Views/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
+using System.Runtime.InteropServices;
+using System.Windows.Interop;
 using Viewer.ViewModels;
 
 namespace Viewer
@@ -8,11 +11,84 @@ namespace Viewer
     /// </summary>
     public partial class MainWindow : Window
     {
-        MainVM vm = new MainVM();
+        readonly MainVM vm = new MainVM();
         public MainWindow()
         {
             InitializeComponent();
             DataContext = vm;
+        }
+
+        #region System Menu
+
+        // Related to managing the System Menu
+        private IntPtr Handle
+        {
+            get { return new WindowInteropHelper(this).Handle; }
+        }
+
+        private const int AboutMenuID = 1000;
+        //private const Int32 ScrapeMenuID = 1001;
+
+        private static IntPtr WndProc(IntPtr hWnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+            // Check if a System Command has been executed
+            if (msg == WM_SYSCOMMAND)
+            {
+                // Execute the appropriate code for the clicked System Menu item
+                switch (wParam.ToInt32())
+                {
+                    case AboutMenuID:
+                        AboutWindow wdw = new AboutWindow();
+                        wdw.ShowDialog();
+                        handled = true;
+                        break;
+                        //case ScrapeMenuID:
+                        //    var content = vm.GetPageHtml(browser.Document);
+                        //    vm.WriteToFile(content);
+                        //    MessageBox.Show("\"Settings\" was clicked");
+                        //    handled = true;
+                        //    break;
+                }
+            }
+
+            return IntPtr.Zero;
+        }
+
+        private void AddMenuItemsToSystemMenu()
+        {
+            // Get the Handle for the Forms System Menu
+            IntPtr systemMenuHandle = GetSystemMenu(this.Handle, false);
+
+            // Create new System Menu items just before the Close menu item
+            InsertMenu(systemMenuHandle, 5, MF_BYPOSITION | MF_SEPARATOR, 0, string.Empty); // separator
+            InsertMenu(systemMenuHandle, 6, MF_BYPOSITION, AboutMenuID, "About");
+            //InsertMenu(systemMenuHandle, 7, MF_BYPOSITION, ScrapeMenuID, "Scrape");
+
+            // Attach the WndProc handler
+            HwndSource source = HwndSource.FromHwnd(this.Handle);
+            source.AddHook(new HwndSourceHook(WndProc));
+        }
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetSystemMenu(IntPtr hWnd, bool bRevert);
+
+        [DllImport("user32.dll")]
+        private static extern bool InsertMenu(IntPtr hMenu, int wPosition, int wFlags, int wIDNewItem, string lpNewItem);
+
+        [DllImport("user32.dll")]
+        private static extern bool AppendMenu(IntPtr hMenu, int wPosition, int wFlags, int wIDNewItem, string lpNewItem);
+
+        // Constants
+        public const int WM_SYSCOMMAND = 0x112;
+        public const int MF_SEPARATOR = 0x800;
+        public const int MF_BYPOSITION = 0x400;
+        public const int MF_STRING = 0x0;
+
+        #endregion
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            AddMenuItemsToSystemMenu();
         }
     }
 }
